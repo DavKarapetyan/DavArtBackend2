6bfba0879219d8d7f2ab491097d4f7f066c5f655
Davit Karapetyan <davit207karapetyan@gmail.com>
2022-10-22 20:47:31 +0400

    asdsadsad

--- DavArt.BLL/Services/Interfaces/IProductInterface.cs
+++ DavArt.BLL/Services/Interfaces/IProductInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DavArt.BLL.ViewModels.Product;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,6 @@ namespace DavArt.BLL.Services.Interfaces
     public interface IProductInterface
     {
         Task Add(ProductViewModel model);
-
+        Task Update(ProductViewModel model)
     }
 }
